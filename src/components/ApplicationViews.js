684fa53c9598dc614f5acadb54f9216369464150
warrior14 <ironaddicts14@gmail.com>
2021-07-20 11:04:12 -0500

    fixed merge conflicts

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,22 +1,18 @@
 import React from "react"
 import { Route } from "react-router-dom"
-<<<<<<< HEAD
 import { MessageProvider } from "./messages/MessageProvider";
 import { MessageList } from "./messages/MessageList";
-=======
 import { FriendsDetails } from "./friends/FriendsDetails"
 import { FriendList } from "./friends/FriendsList"
 import { FriendProvider } from "./friends/FriendsProvider"
 import { FriendsForm } from "./friends/FriendsForm"
 import { UserProvider } from "./users/UsersProvider"
->>>>>>> main
+
 export const ApplicationViews = () => {
   return (
     <>
-
+      <Route>
         {/* Render the component for news articles */}
-<<<<<<< HEAD
-=======
       </Route>
 
       <FriendProvider>
@@ -39,7 +35,6 @@ export const ApplicationViews = () => {
       <Route path="/messages">
         {/* Render the component for the messages */}
       </Route>
->>>>>>> main
 
       <MessageProvider>   
           <Route path="/messages">
