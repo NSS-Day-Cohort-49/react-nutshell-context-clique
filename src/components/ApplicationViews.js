558baf3b7591afe04646515b99f870d952c8e43d
warrior14 <ironaddicts14@gmail.com>
2021-07-20 10:32:07 -0500

    added post and delet funtionality to the public messages

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,39 +1,21 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import { FriendsDetails } from "./friends/FriendsDetails"
-import { FriendList } from "./friends/FriendsList"
-import { FriendProvider } from "./friends/FriendsProvider"
-
+import { MessageProvider } from "./messages/MessageProvider";
+import { MessageList } from "./messages/MessageList";
 export const ApplicationViews = () => {
   return (
     <>
 
-      <Route exact path="/">
         {/* Render the component for news articles */}
-      </Route>
 
-      <FriendProvider>
-        <Route exact path="/friends/detail/:friendId(\d+)">
-          <FriendsDetails />
-        </Route>
+      <MessageProvider>   
+          <Route path="/messages">
+            {/* Render the component for the messages */}
+            <MessageList />
+          </Route>
+      </MessageProvider>
 
-        <Route exact path="/friends">
-          <FriendList />
-        </Route>
-
-      </FriendProvider>
-
-      <Route path="/messages">
-        {/* Render the component for the messages */}
-      </Route>
-
-      <Route path="/tasks">
-        {/* Render the component for the user's tasks */}
-      </Route>
-
-      <Route path="/events">
-        {/* Render the component for the user's events */}
-      </Route>
+      
     </>
   )
 }
