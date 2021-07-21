85169b6cd54d4d4cc3185d83520ff4ba937b9baa
averynoble <averynoble.an@gmail.com>
2021-07-21 11:47:28 -0500

    verified that messages were showing and able to add and delete articles

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,22 +1,27 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { MessageProvider } from "./messages/MessageProvider";
-import { MessageList } from "./messages/MessageList";
-import { FriendsDetails } from "./friends/FriendsDetails"
-import { FriendList } from "./friends/FriendsList"
 import { FriendProvider } from "./friends/FriendsProvider"
+import { ArticleProvider } from "./articles/ArticleProvider";
 import { UserProvider } from "./users/UsersProvider"
+import { ArticleList } from "./articles/ArticleList";
+import { MessageList } from "./messages/MessageList";
+import { FriendList } from "./friends/FriendsList"
+import { FriendsDetails } from "./friends/FriendsDetails"
+import { ArticleForm } from "./articles/ArticleForm";
+import { FriendsForm } from "./friends/FriendsForm";
 
 export const ApplicationViews = () => {
   return (
     <>
       <ArticleProvider>
         <FriendProvider>
-          <Route exact path="/">
-            {/* Render the component for news articles */}
-            <ArticleList />
-          </Route>
-
+          <MessageProvider>
+            <Route exact path="/">
+              {/* Render the component for news articles */}
+              <ArticleList />
+            </Route>
+          </MessageProvider>
 
           <Route exact path="/articles/create">
             <ArticleForm />
